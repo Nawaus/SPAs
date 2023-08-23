600432538d5dc9ea0a49b9215ecd6d254ad10483
unknown <ggug9289@gmail.com>
2023-08-23 17:44:02 -0300

    update

--- src/loaders/products.js
+++ src/loaders/products.js
@@ -2,10 +2,10 @@ import products from "../data/database.json";
 
 export default function loadProducts({ params }) {
   const product = products.find((p) => p.id === +params.productId);
-
-  if (!product) {
-    throw new Response("404 Not found", { status: 404 });
-  }
+  //
+  //   if (!product) {
+  //     throw new Response("404 Not found", { status: 404 });
+  //   }
 
   return product;
 }
